3be37ce6decf8cd732b9b3c6c9bdac218ced2b7c
Iluvmagick <d.tabalin@nil.foundation>
2024-01-31 11:02:21 +0400

    Updated nix config for Mac.

--- shell.nix
+++ shell.nix
@@ -5,6 +5,7 @@ in
 pkgs.mkShell {
   buildInputs = with pkgs; [
     niv
+    git
     cmake
     boost
     pkg-config
@@ -17,23 +18,35 @@ pkgs.mkShell {
     pangomm
     util-linux
     xorg.libXdmcp
-    libselinux
-    libsepol
     pcre
     fribidi
     libthai
     libdatrie
     libdeflate
     wrapGAppsHook4
-    gdb
-    valgrind
-    libsForQt5.kcachegrind
-    linuxKernel.packages.linux_zen.perf
+    lldb
+    #valgrind
+    #libsForQt5.kcachegrind
+    #linuxKernel.packages.linux_zen.perf
     clang-tools
     clang
   ];
 
   shellHook = ''
     export NO_AT_BRIDGE="1"
+    function nil_test_runner() {
+      clear
+      filename=$(cat Makefile | grep "$2" | awk 'NR==1{print $NF}')
+      make -j$(nproc) "$filename" && ./libs/crypto3/libs/$1/test/$filename
+    }
+    function ctcmp() {
+      nil_test_runner blueprint $1
+    }
+    function ztcmp() {
+      nil_test_runner zk $1
+    }
+    function mtcmp() {
+      nil_test_runner math $1
+    }
   '';
 }
