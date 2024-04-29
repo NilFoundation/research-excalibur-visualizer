98677fc91fb2993dec87e406a8d9b5cfbcd5f43d
Iluvmagick <d.tabalin@nil.foundation>
2024-04-29 17:28:26 +0400

    Added bn curve.

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,9 @@
 #include <nil/crypto3/algebra/fields/mnt4/base_field.hpp>
 #include <nil/crypto3/algebra/fields/mnt6/base_field.hpp>
 #include <nil/crypto3/algebra/fields/arithmetic_params/goldilocks64.hpp>
+#include <nil/crypto3/algebra/curves/alt_bn128.hpp>
 
+#include "nil/crypto3/algebra/fields/alt_bn128/scalar_field.hpp"
 #include "table.hpp"
 
 int main(int argc, char* argv[]) {
@@ -52,14 +54,16 @@ int main(int argc, char* argv[]) {
     using mnt4_curve_type = nil::crypto3::algebra::fields::mnt4_fq<298>;
     using mnt6_curve_type = nil::crypto3::algebra::fields::mnt6_fq<298>;
     using goldilocks64_field_type = nil::crypto3::algebra::fields::goldilocks64;
+    using bn_base_field_type = nil::crypto3::algebra::fields::alt_bn128<254>;
+    using bn_scalar_field_type = nil::crypto3::algebra::fields::alt_bn128_scalar_field<254>;
 
     Glib::OptionGroup::vecustrings main_option_vector;
     Glib::OptionGroup main_group("curves", "Curves", "Curve used in the program");
 
     bool vesta = false, pallas = false, bls12_fr_381 = false, bls12_fq_381 = false,
-         mnt4 = false, mnt6 = false, goldilocks64 = false;
+         mnt4 = false, mnt6 = false, goldilocks64 = false, bn_base = false, bn_scalar = false;
     Glib::OptionEntry vesta_entry, pallas_entry, bls12_fr_381_entry, bls12_fq_381_entry,
-                      mnt4_entry, mnt6_entry, goldilocks64_entry;
+                      mnt4_entry, mnt6_entry, goldilocks64_entry, bn_entry;
 
     vesta_entry.set_long_name("vesta");
     vesta_entry.set_short_name('v');
@@ -96,6 +100,16 @@ int main(int argc, char* argv[]) {
     goldilocks64_entry.set_description("Use Goldilocks64 curve");
     main_group.add_entry(goldilocks64_entry, goldilocks64);
 
+    bn_entry.set_long_name("bn");
+    bn_entry.set_short_name('n');
+    bn_entry.set_description("Use BN curve base field");
+    main_group.add_entry(bn_entry, bn_base);
+
+    bn_entry.set_long_name("bn_scalar");
+    bn_entry.set_short_name('s');
+    bn_entry.set_description("Use BN curve scalar field");
+    main_group.add_entry(bn_entry, bn_scalar);
+
     // Add the main group to the context
     Glib::OptionContext context;
     context.set_main_group(main_group);
@@ -105,7 +119,8 @@ int main(int argc, char* argv[]) {
 
     // check that only a single curve is selected
     std::vector<bool> curve_selections = {
-        vesta, pallas, bls12_fr_381, bls12_fq_381,mnt4, mnt6, goldilocks64};
+        vesta, pallas, bls12_fr_381, bls12_fq_381,mnt4, mnt6, goldilocks64,
+        bn_base, bn_scalar};
     uint8_t curve_count = std::accumulate(curve_selections.begin(), curve_selections.end(), 0);
     if (curve_count > 1) {
         std::cerr << "Error: only one curve can be used at a time." << std::endl;
@@ -114,7 +129,7 @@ int main(int argc, char* argv[]) {
     // check that at least one curve is selected
     if (curve_count == 0) {
         std::cerr << "Error: no curve selected. Use --vesta or --pallas or --bls12_fr_381, or --bls12_fq_381"
-                  << " or --mnt4 or --mnt6 or --goldilocks64"
+                  << " or --mnt4 or --mnt6 or --goldilocks64, or --bn, or --bn_scalar."
                   << std::endl;
         return 1;
     }
@@ -139,5 +154,11 @@ int main(int argc, char* argv[]) {
     }
     if (goldilocks64) {
         return app->make_window_and_run<ExcaliburWindow<goldilocks64_field_type>>(argc, argv);
+    }
+    if (bn_base) {
+        return app->make_window_and_run<ExcaliburWindow<bn_base_field_type>>(argc, argv);
+    }
+    if (bn_scalar) {
+        return app->make_window_and_run<ExcaliburWindow<bn_scalar_field_type>>(argc, argv);
     }
 }
